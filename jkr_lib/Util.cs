71e534116fc38ac119eb6000723e60fd22360d6e
nico-apps <nicomarikucza@googlemail.com>
2024-08-12 13:21:25 +0200

    Add AddFilesToArchive method

--- jkr_lib/Util.cs
+++ jkr_lib/Util.cs
@@ -15,4 +15,14 @@ public static class Util {
         var arch = new JKRArchive(data);
         arch.Unpack(new DirectoryInfo(file).Parent!);
     }
+    public static void AddFilesToArchive(string archive, string folderPath, Endian endian = Endian.Big) {
+        var data = File.ReadAllBytes(archive);
+        data = Yaz0.Decompress(data);
+        var arch = new JKRArchive(data);
+        arch.ImportFromFolder(folderPath, JKRFileAttr.FILE | JKRFileAttr.LOAD_TO_MRAM);
+        data = arch.ToBytes(endian);
+        data = Yaz0.Compress(data);
+        FileInfo finfo = new(new DirectoryInfo(archive).FullName);
+        File.WriteAllBytes(finfo.FullName, data);
+    }
 }
